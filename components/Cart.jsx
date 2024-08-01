4ea31aab96699cfcc1a44ea186c3d99c2848ae3b
Matthijs <matthijs.vannistelrooij@gmail.com>
2024-08-02 00:42:12 +0200

    translated english

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -59,14 +59,14 @@ const Cart = () => {
         {cartItems.length < 1 && (
           <div className="empty-cart">
             <AiOutlineShopping size={150} />
-            <h3>Your shopping bag is empty</h3>
+            <h3>Je winkelmandje is nog leeg</h3>
             <Link href="/">
               <button
                 type="button"
                 onClick={() => setShowCart(false)}
                 className="btn"
               >
-                Continue Shopping
+                Terug naar winkelen
               </button>
             </Link>
           </div>
