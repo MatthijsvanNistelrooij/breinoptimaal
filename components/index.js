486f2d4291ecdc34b37c85f6ac38843dfc517863
Matthijs <matthijs.vannistelrooij@gmail.com>
2024-08-01 23:30:23 +0200

    translated english

--- components/index.js
+++ components/index.js
@@ -1,6 +1,4 @@
 export { default as Footer } from './Footer';
 export { default as Navbar } from './Navbar';
 export { default as Product } from './Product';
-export { default as HeroBanner } from './HeroBanner';
-export { default as FooterBanner } from './FooterBanner';
 export { default as Cart } from './Cart';
